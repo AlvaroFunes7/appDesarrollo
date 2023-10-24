8e55507abde54fb55dab6a45af005d2c1f425a43
alvaro.funes <alvarofunestena14@gmail.com>
2023-10-24 20:09:13 +0200

    create structure

--- aplicacionGrupo/src/app/app.module.ts
+++ aplicacionGrupo/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VistaComponent } from './vista/vista.component';
+import { FootterComponent } from './footter/footter.component';
+import { CarPersonComponent } from './car-person/car-person.component';
+import { FormPersonComponent } from './form-person/form-person.component';
+import { CardPersonComponent } from './card-person/card-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VistaComponent,
+    FootterComponent,
+    CarPersonComponent,
+    FormPersonComponent,
+    CardPersonComponent
   ],
   imports: [
     BrowserModule
